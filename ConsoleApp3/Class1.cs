858edab5178e82c8ceeb19442886cfb4f7a45157
Alexey Alexey <alextutor72@gmail.com>
2022-11-07 18:50:39 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public delegate void Finish_time(string t);
+    class Car
+    {
+        public string Model { get; set; }
+        public string Fio { get; set; }
+        public int max_speed { get; set; }
+        public void Time(string time)
+        {
+            Console.WriteLine($"Car {Model} drive {Fio} maxspeed {max_speed} time{time} ");
+        }
+    }
+    class Rally
+    {
+        public event Finish_time finishEvent;
+        public void Finish(string time)
+        {
+            if (finishEvent != null)
+            {
+                finishEvent(time);
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using ConsoleApp3;
+
+static void Main(string[] args)
+        {
+            List<Car> zaezd = new List<Car> {new Car {
+         Model = Console.ReadLine(),
+         Fio = Console.ReadLine(),
+         max_speed = int.Parse(Console.ReadLine())
+         },
+         new Car {
+         Model = Console.ReadLine(),
+         Fio = Console.ReadLine(),
+         max_speed = int.Parse(Console.ReadLine())
+         },
+         new Car {
+         Model = Console.ReadLine(),
+         Fio = Console.ReadLine(),
+         max_speed = int.Parse(Console.ReadLine())
+         },
+         new Car {
+         Model = Console.ReadLine(),
+         Fio = Console.ReadLine(),
+         max_speed = int.Parse(Console.ReadLine())
+         },
+ };
+            Rally Dakar = new Rally();
+            foreach (Car i in zaezd)
+            {
+                Car.finishEvent += i.Time;
+            }
+            teacher.Exam("Math");
+        }
+    }
+}
